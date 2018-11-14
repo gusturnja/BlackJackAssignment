36037a48f651e3fca866b86d99f3ac2274cb22d1
Jack David Turner <up776193@myport.ac.uk>
2018-11-14 12:56:27 +0100

    Fixed a major error with value and cleaned up unecessary functions

--- BlackJack.hs
+++ BlackJack.hs
@@ -19,20 +19,9 @@ empty = Empty
 --A2
 -- | Calculates the lowest possible value of a hand decided on the value of the ace card
 value :: Hand -> Integer
-value hand = if (inivalue > 21) && ((numberOfAces hand) > 1) then inivalue else finalvalue
-  where inivalue = initialValue hand
-        finalvalue = valueWithValueOfAces 1 hand
-
--- | The number of aces within a hand
-numberOfAces :: Hand -> Integer
-numberOfAces Empty                = 0
-numberOfAces (Add (Card Ace _) x) = 1 + numberOfAces x
-numberOfAces (Add _ x)            = numberOfAces x
-
--- | Calculates the value of a hand using the default value of an ace
-initialValue :: Hand -> Integer
-initialValue Empty        = 0
-initialValue (Add card x) = valueCard card + initialValue x
+value hand = if iniValue > 21 then finalValue else iniValue
+  where iniValue = valueWithValueOfAces 11 hand
+        finalValue = valueWithValueOfAces 1 hand
 
 -- | Calculates the value of a hand using a specified value for ace cards
 valueWithValueOfAces :: Integer -> Hand -> Integer
@@ -55,7 +44,10 @@ valueCard (Card rank _) = valueRank rank
 --A3
 -- | Determines whether the player is bust
 gameOver :: Hand -> Bool
-gameOver hand = if (value hand) > 21 then True else False
+gameOver hand = value hand > 21
+
+hand1 =  Add (Card {rank = Ace, suit = Diamonds}) (Add (Card {rank = Numeric 10, suit = Clubs}) (Add (Card {rank = Numeric 5, suit = Spades}) (Add (Card {rank = Ace, suit = Diamonds}) Empty)))
+hand2 =  Add (Card {rank = Numeric 4, suit = Clubs}) (Add (Card {rank = Numeric 2, suit = Clubs}) (Add (Card {rank = Numeric 6, suit = Clubs}) (Add (Card {rank = Numeric 9, suit = Spades}) (Add (Card {rank = Numeric 6, suit = Hearts}) (Add (Card {rank = King, suit = Hearts}) (Add (Card {rank = Numeric 8, suit = Hearts}) (Add (Card {rank = Numeric 7, suit = Hearts}) (Add (Card {rank = Queen, suit = Clubs}) (Add (Card {rank = Ace, suit = Hearts}) (Add (Card {rank = Numeric 2, suit = Clubs}) (Add (Card {rank = Numeric 6, suit = Spades}) (Add (Card {rank = Numeric 9, suit = Diamonds}) (Add (Card {rank = Numeric 10, suit = Clubs}) (Add (Card {rank = Numeric 6, suit = Hearts}) (Add (Card {rank = Numeric 9, suit = Hearts}) (Add (Card {rank = Numeric 4, suit = Diamonds}) Empty))))))))))))))))
 
 --A4
 -- | Decides a winner based on two players hand's value and the type of player
@@ -67,47 +59,49 @@ winner handP handB
   | vB > 21 && vP > 21               = Bank
   where vP = value handP
         vB = value handB
+winner _ _ = error "Unexpected input error"
 
 --B1
 -- | Add a second hand to the end of the first hand
 (<+) :: Hand -> Hand -> Hand
 (<+) Empty hand2            = hand2
 (<+) hand1 Empty            = hand1
-(<+) (Add card Empty) hand2 = (Add card hand2)
-(<+) (Add card x ) hand2    = (Add card ((<+) x hand2))
+(<+) (Add card Empty) hand2 = Add card hand2
+(<+) (Add card x ) hand2    = Add card ((<+) x hand2)
 
 prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
 prop_onTopOf_assoc p1 p2 p3 =
   p1<+(p2<+p3) == (p1<+p2)<+p3
 
 prop_size_onTopOf :: Hand -> Hand -> Bool
-prop_size_onTopOf hand1 hand2 = if (size hand1) + (size hand2) == (size (hand1 <+ hand2)) then True else False
+prop_size_onTopOf hand1 hand2 = size hand1 + size hand2 == size (hand1 <+ hand2)
 
 --B2
 -- | Generates a full deck of all 52 possible cards
 fullDeck :: Hand
-fullDeck = (fullSuit Hearts) <+ (fullSuit Spades) <+ (fullSuit Diamonds) <+ (fullSuit Clubs)
+fullDeck = fullSuit Hearts <+ fullSuit Spades <+ fullSuit Diamonds <+ fullSuit Clubs
 
 -- | Generates all 13 cards belonging to a specified suit
 fullSuit :: Suit -> Hand
-fullSuit suit = (fullNonNumericSuit suit) <+ (numericSuit 2 10 suit)
+fullSuit suit = fullNonNumericSuit suit <+ numericSuit 2 10 suit
 
 -- | Generates a hand of all cards with a non-numeric rank of a specified suit
 fullNonNumericSuit :: Suit -> Hand
-fullNonNumericSuit suit = (Add (Card King suit) (Add (Card Queen suit) (Add (Card Jack suit) (Add (Card Ace suit) Empty))))
+fullNonNumericSuit suit = Add (Card King suit) (Add (Card Queen suit) (Add (Card Jack suit) (Add (Card Ace suit) Empty)))
 
 -- | Generates a hand of numeric cards between specified boundaries of a specified suit
 numericSuit :: Integer -> Integer -> Suit -> Hand
 numericSuit x y suit
-  | x > y = Empty
-  | x <= y = (Add (Card (Numeric x) suit) (numericSuit (x+1) y suit))
+  | x > y         = Empty
+  | x <= y        = Add (Card (Numeric x) suit) (numericSuit (x+1) y suit)
+numericSuit _ _ _ = error "Unexpected input error"
 
 --B3
 -- | Draw the top card off of a deck
 draw :: Hand -> Hand -> (Hand,Hand)
 draw Empty hand           = error "draw : The deck is empty"
-draw (Add (card) x) Empty = (x,(Add (card) Empty))
-draw (Add (card) x) hand  = (x,hand <+ (Add (card) Empty))
+draw (Add (card) x) Empty = x,Add card Empty
+draw (Add (card) x) hand  = x,hand <+ Add card Empty
 
 --B4
 -- | Play as the bank player, starting with an empty hand
@@ -137,14 +131,16 @@ removeCard :: Integer -> Integer -> Hand -> Hand
 removeCard _ _ Empty = Empty
 removeCard index removeAt (Add card x)
   | index == removeAt = x
-  | index < removeAt  = (Add card (removeCard (index+1) removeAt x))
+  | index < removeAt  = Add card (removeCard (index+1) removeAt x)
+removeCard _ _ _ = error "Unexpected input error"
 
 -- | Returns a card from a deck at a specified location
 returnCard :: Integer -> Integer -> Hand -> Hand
 returnCard _ _ Empty = Empty
 returnCard index location (Add card x)
-  | index == location = (Add card Empty)
+  | index == location = Add card Empty
   | index < location  = returnCard (index+1) location x
+returnCard _ _ _ = error "Unexpected input error"
 
 -- | Checks whether a card is within a hand before and after a shuffle
 prop_shuffle_sameCards :: StdGen -> Card -> Hand -> Bool
@@ -156,9 +152,9 @@ belongsTo :: Card -> Hand -> Bool
 c `belongsTo` Empty = False
 c `belongsTo` (Add c' h) = c == c' || c `belongsTo` h
 
-
 --B6
 --Sets up the interface
+implementation :: Interface
 implementation = Interface
   { iEmpty    = empty
   , iFullDeck = fullDeck
